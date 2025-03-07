b324bb9cf00a4943625138e62a8a45937df291a0
Gideonjon <gideonjones63@gmail.com>
2025-03-07 14:16:46 +0100

    splash-screen-and-onboarding-fixed-now

--- app/src/main/java/com/example/cypher/SplashScreen.kt
+++ app/src/main/java/com/example/cypher/SplashScreen.kt
@@ -1,59 +1,51 @@
 package com.example.cypher
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.navigation.fragment.findNavController
+import com.example.cypher.databinding.FragmentSplashScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+// Load animations
+        val logoAnimation =
+            AnimationUtils.loadAnimation(requireContext(), R.anim.combined_logo_animation)
+
+        // Start animations
+        binding.imageView.startAnimation(logoAnimation)
+
+        Handler().postDelayed({
+
+            findNavController().navigate(R.id.action_splashScreen_to_onboardingScreen)
+
+
+        }, 5000)
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SplashScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SplashScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
 }
